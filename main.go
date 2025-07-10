92071d225e66a18f8e977664f1304d01c4e63cf6
Dagim <dagimawiengidawork371@gmail.com>
2025-07-10 17:00:31 +0300

    progress

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+
+	"dagi/goRestAPI.com/models"
+
+	"github.com/gin-gonic/gin"
+)
 func main() {
+server:=gin.Default()
+
+server.GET("/events",getEvents)
+server.POST("/events",createEvent)
+
+server.Run(":1234")
 
 }
+ func getEvents(context *gin.Context)  {
+	events:=models.GetAllEvents()
+	context.JSON(http.StatusOK,events)
+ }
+ func createEvent(context *gin.Context){
+  var event models.Event
+  context.ShouldBindJSON(&event)
+  
+ }
